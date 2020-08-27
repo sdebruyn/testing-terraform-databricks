a73ceddea1e013ec8bb1e3bae2b17ef5675ffed1
Sam Debruyn <sam@debruyn.dev>
2020-08-27 14:33:57 +0200

    add app permissions

--- main.tf
+++ main.tf
@@ -49,6 +49,20 @@ data "azurerm_client_config" "c" {}
 
 resource "azuread_application" "app" {
   name = "tf-sp-${local.name}"
+  required_resource_access {
+    resource_app_id = "e406a681-f3d4-42a8-90b6-c2b029497af1"
+    resource_access {
+      id   = "03e0da56-190b-40ad-a80c-ea378c433f7f"
+      type = "Scope"
+    }
+  }
+  required_resource_access {
+    resource_app_id = "00000003-0000-0000-c000-000000000000"
+    resource_access {
+      id   = "e1fe6dd8-ba31-4d61-89e7-88639da4683d"
+      type = "Scope"
+    }
+  }
 }
 
 resource "azuread_service_principal" "sp" {
